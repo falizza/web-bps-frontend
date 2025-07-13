8ece225f5c35663e96f785c5914de4b83836eb4f
Faliza Maulidina <falizamaulidina1508@gmail.com>
2025-07-13 22:47:29 +0700

    update axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,30 +1,8 @@
-// // src/api/axios.js
-// import axios from "axios";
-// const apiClient = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL + "/api",
-//     withCredentials: true,
-//     headers: {
-//         Accept: "application/json", "Content-Type": "application/json",
-//     },
-// });
-// // Interceptor untuk menambahkan Authorization header dari localStorage
-// apiClient.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem("token");
-//         if (token) {
-//             config.headers["Authorization"] = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
-// export default apiClient;
-
 // src/api/axios.js
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+    baseURL: import.meta.env.VITE_API_URL + "/api",
     withCredentials: true, // buat cookie/session kalau pakai Sanctum, dsb
     headers: {
         Accept: "application/json",
